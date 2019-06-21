3d67e81bc72e78186af0fd3ef2cb5b4fa0560199
xq <xq@12>
2019-06-21 10:54:45 +0800

    切片增加学习len和cap

--- src/ch7/slice_test.go
+++ src/ch7/slice_test.go
@@ -10,3 +10,12 @@ func TestSliceInit(t *testing.T)  {
 	slice1 := []int{1,2,3,4,5,6}
 	t.Log(len(slice1), cap(slice1))
 }
+
+func TestSliceDefine(t *testing.T)  {
+	slice := make([]int, 6, 10)
+	slice = append(slice,1,2,3,4,5,6,7)
+	t.Log(slice[9])
+	t.Logf("len + %d + cap %d",len(slice), cap(slice))
+	slice1 := slice[6:10]
+	t.Logf("len + %d + cap %d",len(slice1), cap(slice1))
+}
