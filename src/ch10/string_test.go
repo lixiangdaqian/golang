3517a3835d82ba4e70db5e097a7160c09c216cb6
xq <xq@12>
2019-06-24 10:53:59 +0800

    字符串常用函数

--- src/ch10/string_test.go
+++ src/ch10/string_test.go
@@ -1,6 +1,10 @@
 package stri_test
 
-import "testing"
+import (
+	"testing"
+	"strings"
+	"strconv"
+)
 
 func TestStringInit(t *testing.T) {
 	str := "中国"
@@ -8,4 +12,22 @@ func TestStringInit(t *testing.T) {
 	r := []rune(str)
 	t.Logf("unicode:%x", r[0])
 	t.Logf("utf8:%x", str)
+}
+
+func TestStringFunc(t *testing.T) {
+	s := "a,b,c,d,e,f,g"
+	//按 , 隔开，返回切片
+	parts := strings.Split(s, ",")
+	t.Log(parts)
+	join := strings.Join(parts, ":")
+	t.Log(join)
+}
+
+func TestStringVert(t *testing.T) {
+	s := strconv.Itoa(10)
+	t.Log("string" + s)
+	if in, err := strconv.Atoi(s); err == nil {
+		t.Log(in + 10)
+	}
+
 }
