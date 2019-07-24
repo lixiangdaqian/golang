11771ff057c6a057263ef71df7284c6645d89730
alexxqli <alexxqli@tencent.com>
2019-07-24 16:28:04 +0800

    golang的panic和recover

---
+++ src/ch17/panic_test.go
@@ -0,0 +1,19 @@
+package panic_test
+
+import (
+	"testing"
+	"fmt"
+	"errors"
+)
+
+func TestPanic(t *testing.T)  {
+	fmt.Println("haha")
+	//os.Exit(10)
+	defer func() {
+		fmt.Println("finally")
+		if err := recover(); err != nil {
+			fmt.Println("panic info are ", err)
+		}
+	}()
+	panic(errors.New("panic 了"))
+}
