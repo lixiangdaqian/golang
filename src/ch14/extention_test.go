c1c2fdcd3803727449746ac514b250410f9f9d34
alexxqli <alexxqli@tencent.com>
2019-07-24 15:12:35 +0800

    go的扩展和复用

---
+++ src/ch14/extention_test.go
@@ -0,0 +1,42 @@
+package extention_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+
+}
+
+func (p *Pet)Speck()  {
+	fmt.Print("...")
+}
+
+func (p *Pet)SpeckTo(somebody string)  {
+	p.Speck()
+	fmt.Println(" ", somebody)
+}
+
+type Dog struct {
+	p *Pet
+}
+
+func (d *Dog)Speck()  {
+	d.p.Speck()
+	fmt.Print("wang")
+}
+
+func (d *Dog)SpeckTo(somebody string)  {
+	d.p.SpeckTo(somebody)
+}
+
+//匿名复用，重写无效，不能重载
+type DogNew struct {
+	Pet
+}
+
+func TestDog(t *testing.T)  {
+	dog := new(DogNew)
+	dog.SpeckTo("haha")
+}
