a7f517a8190a83194e86fecaeb4ea28fa26ef465
alexxqli <alexxqli@tencent.com>
2019-07-25 16:10:27 +0800

    协程初始化学习

---
+++ src/ch19/goroutine_test.go
@@ -0,0 +1,16 @@
+package goroutine_test
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGorutine(t *testing.T)  {
+	for i := 0; i<10; i++  {
+		//值传递，其实是复制了一份数据
+		go func(i int){
+			t.Log(i)
+		}(i)
+		time.Sleep(300 * time.Microsecond)
+	}
+}
