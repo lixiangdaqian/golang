68b67c6b4b5e821ee0e641bef4c7cb897886963b
alexxqli <alexxqli@tencent.com>
2019-08-09 11:58:25 +0800

    等所有结果返回，才给返回结果

---
+++ src/ch25/alldone_test.go
@@ -0,0 +1,33 @@
+package ch25
+
+import (
+	"fmt"
+	"testing"
+	"runtime"
+	"time"
+)
+
+func all_done() int{
+	num := 10
+	ch := make(chan int, num)
+	for i := 0; i < num; i++ {
+		go func(i int) {
+			ch <- i
+			fmt.Printf("all_done %d", i)
+		}(i)
+	}
+	finalRes := 0
+	for i := 0; i < num; i++ {
+		finalRes += <- ch
+	}
+	return finalRes
+
+}
+
+func TestAllDone(t *testing.T)  {
+	t.Log("before gorutine num is ",runtime.NumGoroutine())
+	finalRes := all_done()
+	t.Log("result allDone is ", finalRes)
+	time.Sleep(time.Second)
+	t.Log("after gorutine num is ",runtime.NumGoroutine())
+}
